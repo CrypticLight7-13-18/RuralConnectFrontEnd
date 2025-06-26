53f183214eda493c40711513b3741701e464d52c
CrypticLight7-13-18 <lightu7.13.18@gmail.com>
2025-06-26 20:05:37 +0530

    branch init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ChatPage from './pages/Chat';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import ChatPage from "./pages/Chat";
+import Navbar from "./pages/Nav";
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<h1>About Page</h1>} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
-    </Router>
-  )
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
