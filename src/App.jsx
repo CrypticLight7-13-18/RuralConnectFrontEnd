9e4dcdcc75eeae7cd120a6e2a159dc3d8a4a2c7e
CrypticLight7-13-18 <lightu7.13.18@gmail.com>
2025-06-26 21:35:59 +0530

    merge changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ChatPage from './pages/Chat';
-import MyAppointments from './pages/MyAppointments';
-import Navbar from './pages/Nav';
-import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import ChatPage from "./pages/Chat";
+import MyAppointments from "./pages/MyAppointments";
 import Navbar from "./pages/Nav";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<h1>About Page</h1>} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/my-appointments" element={<MyAppointments/>}/>
-      </Routes>
-    </Router>
-    </>
-  )
       <Navbar />
       <Router>
         <Routes>
           <Route path="/" element={<Auth />} />
           <Route path="/about" element={<h1>About Page</h1>} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/my-appointments" element={<MyAppointments />} />
         </Routes>
       </Router>
     </>
