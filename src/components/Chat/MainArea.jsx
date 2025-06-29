3cb4465791e8df57d7678c642eb1b9baa18cc824
Gokul MR <96484798+CrypticLight7-13-18@users.noreply.github.com>
2025-06-29 10:39:47 +0000

    fixed some sizing issues

--- src/components/Chat/MainArea.jsx
+++ src/components/Chat/MainArea.jsx
@@ -16,7 +16,7 @@ export default function MainArea({
   messagesEndRef,
 }) {
   return (
-    <div className="flex-1 overflow-y-auto max-h-[90.5vh] p-4">
+    <div className="flex-1 overflow-y-auto p-4">
       {currentChat ? (
         <div className="max-w-4xl mx-auto">
           {currentChat.messageHistory.map((msg, index) => (
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -169,7 +169,7 @@ const ChatPage = () => {
         showProfile={showProfile}
       />
       {/* Main Chat Area */}
-      <div className="flex-1 flex flex-col min-h-0">
+      <div className="flex-1 flex flex-col min-h-0 max-h-[90.5vh]">
         {/* Header */}
         <ChatHeader currentChat={currentChat} setSidebarOpen={setSidebarOpen} />
         {/* Chat Messages or Welcome Screen */}
